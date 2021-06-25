9e7ab580e451854a088728d6cb5b5c28f25d7eee
AustinYANyh <1241528539@qq.com>
2021-06-25 16:00:42 +0800

    DMUTestDemo命令的入口类

---
+++ DMUTestDemo/Application.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+
+namespace DMUTest
+{
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class AssociativeSectionUpdater : Autodesk.Revit.UI.IExternalApplication
+    {
+        private AddInId m_thisAppId;
+
+        public Result OnStartup(Autodesk.Revit.UI.UIControlledApplication app)
+        {
+            m_thisAppId = app.ActiveAddInId;
+
+            m_sectionUpdater = new SectionUpdater(m_thisAppId);
+            m_sectionUpdater.Register();
+
+         
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(Autodesk.Revit.UI.UIControlledApplication application)
+        {
+            m_sectionUpdater.UnRegister();
+            return Result.Succeeded;
+        }
+
+        private static SectionUpdater m_sectionUpdater = null;
+    }
+}
